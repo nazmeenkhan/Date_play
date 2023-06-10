fcb22cba4baafe0577ccb20bcef8ac39c4ea745a
Nazmeen Khan <khannazmeen111@gmail.com>
2023-06-10 21:30:29 +0530

    final code with no extra console

--- date.js
+++ date.js
@@ -7,22 +7,17 @@ let question = [
 ]
 let startDate = new Date(question[0].date)
 let endDate = new Date(question[question.length-1].date)
-//console.log(new Date(question[0].date), question[question.length-1].date)
-//endDate.setDate(startDate.getDate() + 10)
- 
-// debug again not all values are pushed
+
 let res = []
 let date = startDate
 let ind = 0
-console.log(startDate, endDate)
+
 while (date <= endDate) {
-  console.log(question, ind, question[ind])
-  console.log((new Date(question[ind].date)).toString(), date.toString(), (new Date(question[ind].date)).toString() === date.toString())
   if((new Date(question[ind].date)).toString() === date.toString()) {
     res.push(question[ind])
     ind = ind+1
   } else {
-    res.push({date: new Date(date), value: 0})
+    res.push({date: (new Date(date)).toISOString(), value: 0})
   }
   date.setDate(date.getDate() + 1)
 }
